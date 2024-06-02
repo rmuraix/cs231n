e5811628c43c7acb4f7a6070bbd50042dd04ff19
rmuraix <35632215+rmuraix@users.noreply.github.com>
2024-06-02 22:59:57 +0900

    docs: translate cnn to Japanese

--- src/assignment2/cs231n/classifiers/cnn.py
+++ src/assignment2/cs231n/classifiers/cnn.py
@@ -1,9 +1,10 @@
 from builtins import object
+
 import numpy as np
 
-from ..layers import *
 from ..fast_layers import *
 from ..layer_utils import *
+from ..layers import *
 
 
 class ThreeLayerConvNet(object):
@@ -15,6 +16,12 @@ class ThreeLayerConvNet(object):
     The network operates on minibatches of data that have shape (N, C, H, W)
     consisting of N images, each with height H and width W and with C input
     channels.
+
+    次のアーキテクチャを持つ 3 層の畳み込みネットワーク:
+
+    conv - relu - 2x2 max pool - affine - relu - affine - softmax
+
+    ネットワークは、高さ H、幅 W の N 個の画像と C 個の入力チャネルで構成される、形状 (N、C、H、W) のデータのミニバッチで動作します。
     """
 
     def __init__(
@@ -41,6 +48,18 @@ class ThreeLayerConvNet(object):
           of weights.
         - reg: Scalar giving L2 regularization strength
         - dtype: numpy datatype to use for computation.
+
+        新しいネットワークを初期化します。
+
+        入力:
+        - input_dim: 入力データのサイズを示すタプル (C、H、W)
+        - num_filters: 畳み込み層で使用するフィルターの数
+        - filter_size: 畳み込み層で使用するフィルターの幅/高さ
+        - hidden_dim: 完全接続の隠し層で使用するユニットの数
+        - num_classes: 最終アフィン層から生成するスコアの数。
+        - weight_scale: 重みのランダム初期化の標準偏差を示すスカラー。
+        - reg: L2 正則化の強度を示すスカラー
+        - dtype: 計算に使用する numpy データ型。
         """
         self.params = {}
         self.reg = reg
@@ -60,22 +79,36 @@ class ThreeLayerConvNet(object):
         # and stride of the first convolutional layer are chosen so that           #
         # **the width and height of the input are preserved**. Take a look at      #
         # the start of the loss() function to see how that happens.                #
+        #                                                                          #
+        # 3 層畳み込みネットワークの重みとバイアスを初期化します。
+        # 重みは、標準偏差が weight_scale に等しい 0.0 を中心とする
+        # ガウス分布から初期化する必要があります。
+        # バイアスは 0 に初期化する必要があります。
+        # すべての重みとバイアスは、辞書 self.params に格納する必要があります。
+        # 畳み込み層の重みとバイアスは、キー 'W1' と 'b1' を使用して格納します。
+        # 隠しアフィン層の重みとバイアスにはキー 'W2' と 'b2' を使用し、
+        # 出力アフィン層の重みとバイアスにはキー 'W3' と 'b3' を使用します。
+        #
+        # 重要: この課題では、**入力の幅と高さが保持される** ように
+        # 最初の畳み込み層のパディングとストライドが選択されていると想定できます。
+        # loss() 関数の先頭を見て、それがどのように行われるかを確認してください。
         ############################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-        F, (C, H, W) = num_filters, input_dim  # dim size
+        # 次元サイズの取得
+        F, (C, H, W) = num_filters, input_dim
 
         self.params.update(
             {
                 "W1": np.random.randn(F, C, filter_size, filter_size)
-                * weight_scale,  # consider all filter shapes
-                "b1": np.zeros(num_filters),  # consider number of filters
+                * weight_scale,  # すべてのフィルタ形状を考慮する
+                "b1": np.zeros(num_filters),  # フィルターの数を考慮する
                 "W2": np.random.randn(F * H * W // 4, hidden_dim)
-                * weight_scale,  # consider reduced pool output
-                "b2": np.zeros(hidden_dim),  # consider number of hidden nodes
+                * weight_scale,  # プール出力の削減を検討する
+                "b2": np.zeros(hidden_dim),  # 隠れノードの数を考慮する
                 "W3": np.random.randn(hidden_dim, num_classes)
-                * weight_scale,  # consider hiddden and output nodes
-                "b3": np.zeros(num_classes),  # consider output nodes
+                * weight_scale,  # 隠しノードと出力ノードを考慮する
+                "b3": np.zeros(num_classes),  # 出力ノードを考慮する
             }
         )
 
@@ -92,13 +125,17 @@ class ThreeLayerConvNet(object):
         Evaluate loss and gradient for the three-layer convolutional network.
 
         Input / output: Same API as TwoLayerNet in fc_net.py.
+
+        3 層畳み込みネットワークの損失と勾配を評価します。
+
+        入力 / 出力: fc_net.py の TwoLayerNet と同じ API。
         """
         W1, b1 = self.params["W1"], self.params["b1"]
         W2, b2 = self.params["W2"], self.params["b2"]
         W3, b3 = self.params["W3"], self.params["b3"]
 
-        # pass conv_param to the forward pass for the convolutional layer
-        # Padding and stride chosen to preserve the input spatial size
+        # conv_paramを畳み込み層のフォワードパスに渡す
+        # 入力空間サイズを維持するために選択されたパディングとストライド
         filter_size = W1.shape[2]
         conv_param = {"stride": 1, "pad": (filter_size - 1) // 2}
 
@@ -113,14 +150,21 @@ class ThreeLayerConvNet(object):
         #                                                                          #
         # Remember you can use the functions defined in cs231n/fast_layers.py and  #
         # cs231n/layer_utils.py in your implementation (already imported).         #
+        #                                                                          #
+        # 3 層畳み込みネットワークのフォワードパスを実装し、X のクラススコアを計算し、
+        # scores 変数に格納します。
+        #
+        # 実装には、cs231n/fast_layers.py と cs231n/layer_utils.py で定義された
+        # 関数を使用できることを覚えておいてください（すでにインポートされています）。
         ############################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-        out, cache1 = conv_relu_pool_forward(
-            X, W1, b1, conv_param, pool_param
-        )  # CONV forward pass
-        out, cache2 = affine_relu_forward(out, W2, b2)  # FC forward pass
-        scores, cache3 = generic_forward(out, W3, b3, last=True)  # last forward pass
+        # 畳み込みフォワードパス
+        out, cache1 = conv_relu_pool_forward(X, W1, b1, conv_param, pool_param)
+        # 全結合フォワードパス
+        out, cache2 = affine_relu_forward(out, W2, b2)
+        # 出力層フォワードパス
+        scores, cache3 = generic_forward(out, W3, b3, last=True)
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         ############################################################################
@@ -140,31 +184,41 @@ class ThreeLayerConvNet(object):
         # NOTE: To ensure that your implementation matches ours and you pass the   #
         # automated tests, make sure that your L2 regularization includes a factor #
         # of 0.5 to simplify the expression for the gradient.                      #
+        #                                                                          #
+        # 3 層畳み込みネットワークのバックワードパスを実装し、
+        # 損失と勾配を loss と grads 変数に格納します。
+        # softmax を使用してデータ損失を計算し、
+        # grads[k] が self.params[k] の勾配を保持するようにします。
+        # L2 正則化を追加することを忘れないでください！
+        #
+        # 注: 実装が私たちのものと一致し、自動テストに合格するようにするには、
+        # L2 正則化に 0.5 の係数を含めるようにして、
+        # 勾配の式を簡略化してください。
         ############################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-        loss, dout = softmax_loss(scores, y)  # loss and dout
-        loss += (
-            0.5 * self.reg * (np.sum(W1**2) + np.sum(W2**2) + np.sum(W3**2))
-        )  # regularized loss
-
-        dout, grads["W3"], grads["b3"], _, _ = generic_backward(
-            dout, cache3
-        )  # first backward pass
+        loss, dout = softmax_loss(scores, y)
+        # 正規化された損失
+        loss += 0.5 * self.reg * (np.sum(W1**2) + np.sum(W2**2) + np.sum(W3**2))
+        # 最初のバックワードパス
+        dout, grads["W3"], grads["b3"], _, _ = generic_backward(dout, cache3)
+        # 全結合バックワードパス
         (
             dout,
             grads["W2"],
             grads["b2"],
-        ) = affine_relu_backward(dout, cache2)  # FC backward pass
+        ) = affine_relu_backward(dout, cache2)
+        # 畳み込みバックワードパス
         (
             dout,
             grads["W1"],
             grads["b1"],
-        ) = conv_relu_pool_backward(dout, cache1)  # CONV backward pass
+        ) = conv_relu_pool_backward(dout, cache1)
 
-        grads["W3"] += self.reg * W3  # L2 for W3
-        grads["W2"] += self.reg * W2  # L2 for W2
-        grads["W1"] += self.reg * W1  # L2 for W1
+        # L2 正則化
+        grads["W3"] += self.reg * W3
+        grads["W2"] += self.reg * W2
+        grads["W1"] += self.reg * W1
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         ############################################################################
